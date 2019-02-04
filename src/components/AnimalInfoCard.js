33816fdaf5464cfe6993e0e967ec49d7683dae75
github username <raquel.sae.randall@gmail.com>
2019-02-03 19:51:36 -0500

    added style to animal and plant info

--- src/components/AnimalInfoCard.js
+++ src/components/AnimalInfoCard.js
@@ -1,24 +1,95 @@
 import React from 'react';
-
+import { Accordion, Icon, Segment, Image } from 'semantic-ui-react'
 
 class AnimalInfoCard extends React.Component {
+  state = { activeIndex: 0 }
 
+    handleClick = (e, titleProps) => {
+      const { index } = titleProps
+      const { activeIndex } = this.state
+      const newIndex = activeIndex === index ? -1 : index
+
+      this.setState({ activeIndex: newIndex })
+    }
   render(){
+    const { activeIndex } = this.state
     let {animal} = this.props
     return(
-      <div>
-      <ul>
-        <img src={animal.image} alt="" />
-        <li>Species: {animal.species}</li>
-        <li>Size: {animal.size}</li>
-        <li>Lifespan: {animal.lifespan}</li>
-        <li>Location: {animal.location}</li>
-        <li>Prey: {animal.prey}</li>
-        <li>Predators: {animal.predators}</li>
-        <li>Interesting Fact: {animal.other_info}</li>
-      </ul>
-      <hr />
-      </div>
+
+      <Segment>
+         <Image src={animal.image} size='huge' centered />
+
+      <Accordion fluid styled>
+        <Accordion.Title active={activeIndex === 0} index={0} onClick={this.handleClick}>
+          <Icon name='dropdown' />
+          Species
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 0}>
+          <p>
+            {animal.species}
+          </p>
+        </Accordion.Content>
+
+        <Accordion.Title active={activeIndex === 1} index={1} onClick={this.handleClick}>
+          <Icon name='dropdown' />
+          Size
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 1}>
+          <p>
+            {animal.size}
+          </p>
+        </Accordion.Content>
+
+        <Accordion.Title active={activeIndex === 2} index={2} onClick={this.handleClick}>
+          <Icon name='dropdown' />
+          Lifespan
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 2}>
+          <p>
+            {animal.lifespan}
+          </p>
+        </Accordion.Content>
+
+        <Accordion.Title active={activeIndex === 3} index={3} onClick={this.handleClick}>
+          <Icon name='dropdown' />
+          Location
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 3}>
+          <p>
+            {animal.location}
+          </p>
+        </Accordion.Content>
+
+        <Accordion.Title active={activeIndex === 4} index={4} onClick={this.handleClick}>
+          <Icon name='dropdown' />
+          Prey
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 4}>
+          <p>
+            {animal.prey}
+          </p>
+        </Accordion.Content>
+        <Accordion.Title active={activeIndex === 5} index={5} onClick={this.handleClick}>
+          <Icon name='dropdown' />
+          Predators
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 5}>
+          <p>
+            {animal.predators}
+          </p>
+        </Accordion.Content>
+
+        <Accordion.Title active={activeIndex === 6} index={6} onClick={this.handleClick}>
+          <Icon name='dropdown' />
+          Interesting Fact
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 6}>
+          <p>
+            {animal.other_info}
+          </p>
+        </Accordion.Content>
+      </Accordion>
+      </Segment>
     )
   }
 }
--- src/components/PlantInfoCard.js
+++ src/components/PlantInfoCard.js
@@ -1,23 +1,85 @@
 import React from 'react';
-
+import { Accordion, Icon, Segment, Image } from 'semantic-ui-react'
 
 class PlantInfoCard extends React.Component {
+  state = { activeIndex: 0 }
 
+    handleClick = (e, titleProps) => {
+      const { index } = titleProps
+      const { activeIndex } = this.state
+      const newIndex = activeIndex === index ? -1 : index
+
+      this.setState({ activeIndex: newIndex })
+    }
   render(){
+    const { activeIndex } = this.state
     let {plant} = this.props
     return(
-      <div>
-      <ul>
-        <img src={plant.image} alt="" />
-        <li>Species: {plant.species}</li>
-        <li>Size: {plant.size}</li>
-        <li>Seed Distribution: {plant.seed_distribution}</li>
-        <li>Location: {plant.location}</li>
-        <li>Predators: {plant.predators}</li>
-        <li>Interesting Fact: {plant.other_info}</li>
-      </ul>
-      <hr />
-      </div>
+      <Segment>
+         <Image src={plant.image} size='huge' centered />
+
+      <Accordion fluid styled>
+        <Accordion.Title active={activeIndex === 0} index={0} onClick={this.handleClick}>
+          <Icon name='dropdown' />
+          Species
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 0}>
+          <p>
+            {plant.species}
+          </p>
+        </Accordion.Content>
+
+        <Accordion.Title active={activeIndex === 1} index={1} onClick={this.handleClick}>
+          <Icon name='dropdown' />
+          Size
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 1}>
+          <p>
+            {plant.size}
+          </p>
+        </Accordion.Content>
+
+        <Accordion.Title active={activeIndex === 2} index={2} onClick={this.handleClick}>
+          <Icon name='dropdown' />
+          Seed Distribution
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 2}>
+          <p>
+            {plant.seed_distribution}
+          </p>
+        </Accordion.Content>
+
+        <Accordion.Title active={activeIndex === 3} index={3} onClick={this.handleClick}>
+          <Icon name='dropdown' />
+          Location
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 3}>
+          <p>
+            {plant.location}
+          </p>
+        </Accordion.Content>
+
+        <Accordion.Title active={activeIndex === 4} index={4} onClick={this.handleClick}>
+          <Icon name='dropdown' />
+          Predators
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 4}>
+          <p>
+            {plant.predators}
+          </p>
+        </Accordion.Content>
+
+        <Accordion.Title active={activeIndex === 5} index={5} onClick={this.handleClick}>
+          <Icon name='dropdown' />
+          Interesting Fact
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 5}>
+          <p>
+            {plant.other_info}
+          </p>
+        </Accordion.Content>
+      </Accordion>
+      </Segment>
     )
   }
 }
