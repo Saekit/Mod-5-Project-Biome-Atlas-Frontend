223761f35c6da7f47851d6b9d35e78e22002610b
github username <raquel.sae.randall@gmail.com>
2019-02-10 11:46:54 -0500

    added new animal

--- src/containers/QuizContainer.js
+++ src/containers/QuizContainer.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import QuizCard from '../components/QuizCard'
-import {Form } from 'semantic-ui-react'
+import {Form, Container} from 'semantic-ui-react'
 
+const answerKey = {
+  1: "Taiga",
+  2: "Tundra",
+  3: "Aquatic",
+  4: "Desert",
+  5: "Rainforest",
+  6: "Dumbo Octopus",
+  7: "Lemming",
+  8: "Armadillo Girdled Lizard",
+  9: "Okapi",
+  10: "Red-Crowned Crane"
+}
 
 class QuizContainer extends Component {
 
-  state = {}
+  state = {
+    score: 0,
+    showForm: "hidden",
+  }
 
   idHandler = (id, answer) => {
     this.setState({
@@ -13,20 +28,47 @@ class QuizContainer extends Component {
     })
   }
 
-  submitHandler = () => {
-    
+  submitHandler = (e) => {
+    let length = Object.keys(answerKey).length
+    let i = 0;
+    while (i < length){
+      if (answerKey[i+1] === this.state[i+1]){
+        this.setState({
+          score: this.state.score += 1,
+        })
+      }
+      }
+      i++;
+    this.setState({
+      showForm: "show"
+    })
+    return this.state.score
+  }
+
+  clickHandler = () => {
+    window.location.reload()
   }
 
 
   render(){
-    console.log(this.state);
+
     let qa = this.props.questionsAnswers.map((qa, idx)=> {
       return <QuizCard key={idx} qa={qa} idHandler={this.idHandler} /> })
+
     return(
+      <Container fluid >
+
+      <div className={this.state.showForm}>
+        <h3>You got {this.state.score}/10!</h3>
+        <button onClick={this.clickHandler}>Retake Quiz</button>
+      </div>
+
       <Form onSubmit={this.submitHandler}>
         <h3>{qa}</h3>
         <Form.Button>Submit</Form.Button>
       </Form>
+
+      </Container>
     )
   }
 }
